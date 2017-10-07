e688172f28a68a598009aac055b7862569687f6f
shkelmark <maggie29204@gmail.com>
2017-10-07 18:03:13 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double a;
+        int count;
+        bool operate = true;
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 0;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ",";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 1;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 2;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 3;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 4;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 5;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 6;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 7;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 8;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + 9;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(operate==true)
+            {
+                textBox1.Text = "-" + textBox1.Text;
+                operate = false;
+            }
+            else if (operate==false)
+            {
+                textBox1.Text=textBox1.Text.Replace("-", "");
+                operate = true;
+            }           
+        }
+
+        private void calculate()
+        {
+            Maths MyMath = new Maths();
+
+            textBox1.Text = Convert.ToString(MyMath.Calculate(a, double.Parse(textBox1.Text), count));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 1;
+            label1.Text = a.ToString() + "+";
+            operate = true;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 2;
+            label1.Text = a.ToString() + "-";
+            operate = true;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 3;
+            label1.Text = a.ToString() + "*";
+            operate = true;
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            a = float.Parse(textBox1.Text);
+            textBox1.Clear();
+            count = 4;
+            label1.Text = a.ToString() + "/";
+            operate = true;
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            calculate();
+            label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            label1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int lenght = textBox1.Text.Length - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < lenght; i++)
+            {
+                textBox1.Text = textBox1.Text + text[i];
+            }
+        }       
+    }
+}
---
+++ Math.cs
@@ -0,0 +1,52 @@
+﻿namespace Calculator
+{
+    public class Maths
+    {
+        private double Add(double A, double B)
+        {
+            return A + B;
+        }
+
+        private double Sub(double A, double B)
+        {
+            return A - B;
+        }
+
+        private double Mul(double A, double B)
+        {
+            return A * B;
+        }
+
+        private double Div(double A, double B)
+        {
+            return A / B;
+        }
+
+        public double Calculate(double A, double B, int Operate)
+        {
+            double result = 0;
+
+            switch (Operate)
+            {
+                case 1:
+                    result = Add(A, B);
+                    break;
+
+                case 2:
+                    result = Sub(A, B);
+                    break;
+
+                case 3:
+                    result = Mul(A, B);
+                    break;
+
+                case 4:
+                    result = Div(A, B);
+                    break;
+            }
+
+            return result;
+        }
+
+    }
+}
